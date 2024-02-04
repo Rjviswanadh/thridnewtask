28ac820ef1e7aafa49429438abe5f1825f351e83
Rjviswanadh <96373360+Rjviswanadh@users.noreply.github.com>
2024-02-04 23:29:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const alertboxEl = document.getElementById("alertbox");
+
+function alertbox() {
+  alertboxEl.style.display = "block";
+  // alertboxEl.classList.add("alert-change");
+}
+
+function hidealert() {
+  alertboxEl.style.display = "none";
+}
